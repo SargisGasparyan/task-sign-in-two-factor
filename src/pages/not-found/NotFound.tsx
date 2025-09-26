86a91ace7dfc000bf4f5bf227175a97f60f4be7b
SargisGasparyan <sargisgasparyan081991@gmail.com>
2025-09-26 21:59:35 +0400

    Update not-found page

--- src/pages/not-found/NotFound.tsx
+++ src/pages/not-found/NotFound.tsx
@@ -6,9 +6,9 @@ const NotFound: React.FC = () => {
   return (
     <section className={styles.wrapper}>
       <h1 className={styles.code}>404</h1>
-      <p className={styles.text}>Страница не найдена</p>
+      <p className={styles.text}>Page not found</p>
       <Link to="/" className={styles.link}>
-        На главную
+        Go to home page
       </Link>
     </section>
   );
